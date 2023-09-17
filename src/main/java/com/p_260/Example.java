561d8bc9cd2314c373435ee49304596e7e8fee49
VIDESH MAKWANA <videsh.m@brilworks.com>
2023-09-17 18:06:25 +0530

    p_260 : Can we start a thread two times in Java?

---
+++ src/main/java/com/p_260/Example.java
@@ -0,0 +1,14 @@
+package com.p_260;
+
+public class Example {
+    //Question:  Can we start a thread two times in Java?
+    //Answer:    No, after starting a thread, it can never be started again.
+    // If you does so, an IllegalThreadStateException is thrown.
+    // In such case, thread will run once but for second time, it will throw exception.
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        t.start();
+        t.start();
+        System.out.println("Hello World!");
+    }
+}
--- src/main/java/com0/practical_1/demo.java
+++
@@ -1,7 +0,0 @@
-package com0.practical_1;
-
-public class demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
